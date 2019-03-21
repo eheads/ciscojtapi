1db59f7a178aefc6c3d7589619b00e6fb83a5a3d
william <william.atienza@nexlabs.com>
2019-03-21 14:34:33 +0800

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+### Cisco JTAPI Sample App ###
---
+++ src/main/java/com/company/jtapi/ciscojtapi/CiscojtapiApplication.java
@@ -0,0 +1,13 @@
+package com.company.jtapi.ciscojtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiscojtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiscojtapiApplication.class, args);
+	}
+
+}
